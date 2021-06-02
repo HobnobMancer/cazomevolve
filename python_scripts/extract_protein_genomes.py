babe6c4272997b7f11e67184af017b4d9943a0b9
HobnobMancer <eemh1@st-andrews.ac.ul>
2021-06-02 22:28:56 +0100

    add functions for retrieve protein seq and writing to fasta for singl ortholog search

--- python_scripts/extract_protein_genomes.py
+++ python_scripts/extract_protein_genomes.py
@@ -80,6 +80,9 @@ def main(argv: Optional[List[str]] = None, logger: Optional[logging.Logger] = No
     # get paths to genomic assemblies
     genomic_assembly_paths = get_genomic_assembly_paths(args)
 
+    # create fasta files
+    for assembly_path in genomic_assembly_paths:
+        compile_fasta(assembly_path, args)
 
 
 def genomic_assembly_paths(args):
@@ -100,8 +103,8 @@ def genomic_assembly_paths(args):
 
     # retrieve only gbk_files
     for entry in files_in_entries:
-        if entry.name.endswith("genomic.gbff.gz")
-        gbk_files.append(entry)
+        if entry.name.endswith("genomic.gbff.gz"):
+            gbk_files.append(entry)
 
     if len(gbk_files) == 0:
         logger.error(
@@ -111,3 +114,86 @@ def genomic_assembly_paths(args):
         sys.exit(1)
     
     return gbk_files
+
+
+def compile_fasta(assembly_path, args):
+    """Create a fasta file of the proteins contained in the genomic assembly.
+
+    :param assembly_path: Path to genomic assembly
+    :param args: cmd-line args parser
+
+    Return nothing.
+    """
+    logger = logging.getLogger(__name__)
+
+    # compile fasta name species.fasta
+    name_fragments = (assembly_path.name).split("_")
+    genomic_accession = name_fragments[0] + name_fragments[1]
+
+    species = ""
+
+    with gzip.open(assembly_path, "rt") as handle:
+        for gb_record in SeqIO.parse(handle, "genbank"):
+            for (index, feature) in enumerate(gb_record.features):
+                if feature.type == "source":
+                    species = get_record_feature(feature, "organism", genomic_accession)
+                    if species != "":
+                        break
+            if species != "":
+                break
+
+    fasta_path = f"{species}.fasta"
+
+    output_path = args.output_dir / fasta_path
+
+    protein_count = 0
+
+    with open(output_path, "a") as fh:
+        with gzip.open(assembly_path, "rt") as handle:  # unzip the genomic assembly
+            for gb_record in SeqIO.parse(handle, "genbank"):
+                for (index, feature) in enumerate(gb_record.features):
+                    # Parse over only protein encoding features (type = 'CDS')
+                    if feature.type == "CDS":
+                        protein_accession = get_record_feature(feature, "protein_id", "")
+
+                        product = get_record_feature(feature, "product", protein_accession)
+
+                        gene_id = get_record_feature(feature, "protein_id", protein_accession)
+
+                        data_line = f">gi|{gene_id}|gbk|{protein_accession}| {product} [{species}]"
+
+                        seq = get_record_feature(feature, "translation", protein_accession)
+                        if seq == "":
+                            continue
+
+                        seq = "\n".join([seq[i : i + 60] for i in range(0, len(seq), 60)])
+                        file_content = f">{data_line}\n{seq}\n"
+                        fh.write(file_content)
+
+                        protein_count += 1
+    
+    logger.warning(f"{protein_count} proteins in genomic assembly {genomic_accession}")
+
+    return
+
+
+def get_record_feature(feature, qualifier, accession):
+    """Retrieve data from BioPython feature object.
+
+    :param feature: BioPython feature object representing the curernt working protein
+    :param qualifier: str, data to be retrieved
+    :accession_number: str, accession of the protein being parsed.
+
+    Return feature data.
+    """
+    logger = logging.getLogger(__name__)
+
+    try:
+        data = feature.qualifiers[qualifier][0]
+        return data
+    except KeyError:
+        logger.warning(
+            f"Failed to retrieve feature {qualifier}, for protein {accession}\n"
+            "Returning an empty string it its place"
+        )
+        return ""

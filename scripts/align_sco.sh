0721930b7e59d57302966bbd4a5978562f89fa1e
HobnobMancer <eemh1@st-andrews.ac.ul>
2021-06-04 20:59:28 +0100

    add not of where input maybe found

--- scripts/align_sco.sh
+++ scripts/align_sco.sh
@@ -6,13 +6,14 @@
 
 # $1 output directory
 # $2 directory containing fasta files from orthofinder
+# $2 maybe something like orthologues/Results_May28/Single_Copy_Orthologue_Sequences
 # $3 number of threads to use (MAFFT defaults to 1)
 
 # Create output directory
 mkdir -p $1
 
 # Align each set of SCOs
-# $2 maybe something like orthologues/Results_May28/Single_Copy_Orthologue_Sequences
+
 for fname in $2/*.fa
 do
     mafft --thread $3 ${fname} > $1/`basename ${fname%%.fa}`_aligned.fasta

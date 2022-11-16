1715d5a5111b135fffc8e18e7a64436e146e5e6b
HobnobMancer <eemh1@st-andrews.ac.uk>
2022-11-16 15:44:16 +0000

    add bash script to gen fasta files of prot seqs

---
+++ cazomevolve/seq_diversity/get_fam_seqs.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+#
+# (c) University of St Andrews 2020-2021
+# (c) University of Strathclyde 2020-2021
+# (c) James Hutton Institute 2020-2021
+#
+# Author:
+# Emma E. M. Hobbs
+#
+# Contact
+# eemh1@st-andrews.ac.uk
+#
+# Emma E. M. Hobbs,
+# Biomolecular Sciences Building,
+# University of St Andrews,
+# North Haugh Campus,
+# St Andrews,
+# KY16 9ST
+# Scotland,
+# UK
+#
+# The MIT License
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# run_diamond.sh
+
+# $1 email
+# $2 local cazyme db
+# $3 family name(s)
+# $4 output dir (do not include terminal /)
+
+mkdir -p $4
+
+# get the seqs from NCBI
+cw_get_genbank_seqs $2 $1 --families $3
+
+IFS=',' read -ra user_families <<< "$3"
+
+for FAM in "${user_families[@]}"
+do
+    cw_extract_db_seqs $2 genbank $FAM --fasta_file $4/$FAM".seqs.fasta"
+done

b36da80160a2843600cd36d048634612813e3bbd
HobnobMancer <eemh1@st-andrews.ac.uk>
2023-03-31 10:47:45 +0000

    add func to calc proteome size

--- cazomevolve/cazome/explore/cazome_sizes.py
+++ cazomevolve/cazome/explore/cazome_sizes.py
@@ -46,6 +46,121 @@ import numpy as np
 from tqdm import tqdm
 
 
+def count_items_in_cazome(gfp_df, item, grp, round_by=None):
+    """Count the number of unique items per genome and per specificed tax grouping
+    
+    :param gfp_df: panda df, cols = ['Family', 'Genome', 'Protein', 'tax grp', 'tax grp'...]
+    :param item: str, name of column to calculate incidence for, e.g. 'Protein' or 'Family'
+    :param grp: str, name of column to group genomes by
+    :param round_by: int, number of figures to round mean and sd by. If None do not round
+    
+    Return
+    * dict of {genus: {genome: {'families': {families}, 'numOffamilies': int(num of fams)}}}
+    * df, cols = []
+    """
+    cazome_sizes = {}  # {genus: {genome: {'proteins': unique prot acc, 'numOfcazymes': int(num of prots)}}}
+
+    for ri in tqdm(range(len(gfp_df)), desc="Gathering CAZy families per genome"):
+        row = gfp_df.iloc[ri]
+        genome = row['Genome']
+        row_item = row[item]
+        grp_name = row[grp]
+
+        try:
+            cazome_sizes[grp_name]
+        except KeyError:
+            cazome_sizes[grp_name] = {}
+
+        try:
+            cazome_sizes[grp_name][genome][f'{item}s'].add(row_item)
+        except KeyError:
+            cazome_sizes[grp_name][genome] = {f"{item}s": {row_item}}
+            
+    cazyme_size_data = []
+
+    for grp_name in tqdm(cazome_sizes, desc=f"Calculating num of {item} per genome and per {grp}"):
+        for genome in cazome_sizes[grp_name]:
+            num_of_items = len(cazome_sizes[grp_name][genome][f'{item}s'])
+            cazome_sizes[grp_name][genome][f'numOf{item}s'] = num_of_items
+            
+        num_of_items = []
+        for genome in cazome_sizes[grp_name]:
+            num_of_items.append(cazome_sizes[grp_name][genome][f'numOf{item}s'])
+
+        mean_items = np.mean(num_of_items)
+        sd_items = np.std(num_of_items)
+        
+        if round_by is not None:
+            mean_items = mean_items.round(decimals = round_by)
+            sd_items = sd_items.round(decimals = round_by)
+        
+        num_genomes = len(list(cazome_sizes[grp_name].keys()))
+        
+        new_row = [grp_name, mean_items, sd_items, num_genomes]
+        cazyme_size_data.append(new_row)
+    
+    cols = ['Genome', f'Mean{item}s', f'Sd{item}s', 'NumOfGenomes']
+    cazome_size_df = pd.DataFrame(cazyme_size_data, columns=cols)
+    
+    return cazome_sizes, cazome_size_df
+
+
+def get_proteome_sizes(proteome_dir, gfp_df, grp):
+    """Count the number of proteins in the proteome file of each genome
+    
+    Build a dict of proteome sizes grouped by tax lineage ('grp')
+    
+    :param proteome_dir: Path or str, path to dir containing .faa proteome files
+    :param gfp_df: pandas df containing families, genomes, tax_rank, tank_rank...
+    :param grp: str, name of column (tax_rank) to group genomes by, e.g. 'Genus'
+    
+    Return dict {grp: {genome: {'numOfproteins': int()}}}"""
+    proteome_files = get_file_paths(proteome_dir, suffixes=['.faa'])
+
+    proteome_sizes = {}  # {grp: {genome: {'numOfproteins': int()}}}
+
+    for fasta_path in tqdm(proteome_files, desc="Getting proteome sizes"):
+        try:
+            genome = re.findall(r"GCF_\d+\.\d{1,5}", fasta_path.name)[0]
+        except IndexError:
+            try:
+                genome = re.findall(r"GCA_\d+\.\d{1,5}", fasta_path.name)[0]
+            except IndexError:
+                print(
+                    f"Could not retrieve genomic accession from\n{fasta_path}\n"
+                    "Skipping FASTA file"
+                )
+                continue
+
+        # get tax group
+        tax_row = gfp_df[gfp_df['Genome'] == genome]
+        if len(tax_row) == 0:
+            print(
+                f"Genome {genome} was not in the FGP file."
+                "Therefore, skipping proteome proporiton count for genome"
+            )
+            continue
+        tax_group = tax_row[grp].values[0]
+
+        num_of_proteins = 0
+        for record in SeqIO.parse(fasta_path, 'fasta'):
+            num_of_proteins += 1
+
+        try:
+            proteome_sizes[tax_group]
+        except KeyError:
+            proteome_sizes[tax_group] = {}
+
+        proteome_sizes[tax_group][genome] = {'numOfProteins': num_of_proteins}
+
+    return proteome_sizes
+
+
+#
+##
+#
+
+
 def get_cazome_size_df(cazomes_sizes, group_by):
     """Build a df with the mean and SD number of CAZymes per group
     
